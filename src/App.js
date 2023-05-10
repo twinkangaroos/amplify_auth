5b829bf099a978456f4dd5b709de4efffbc5bde2
twinkangaroos <132538451+twinkangaroos@users.noreply.github.com>
2023-05-10 19:27:56 +0900

    Japanese language support

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
-import { Amplify } from 'aws-amplify';
+import { Amplify, I18n } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import UserEdit from "./pages/UserEdit";
 
+import { Authenticator, translations } from '@aws-amplify/ui-react';
+
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
+// https://ui.docs.amplify.aws/react/connected-components/authenticator/customization#internationalization-i18n
+I18n.putVocabularies(translations);
+I18n.setLanguage('ja');
+
+// https://github.com/aws-amplify/amplify-ui/blob/main/packages/ui/src/i18n/dictionaries/authenticator/en.ts
+I18n.putVocabularies({
+  ja: {
+    'Sign in': 'ログインする',
+    'Sign In': 'ログイン',
+    'Sign Up': '会員登録する',
+    'Create Account': '会員登録',
+    'Email': 'メールアドレス',
+    'Enter your Email': 'メールアドレスを入力してください',
+    'Enter your Password': 'パスワードを入力してください',
+    'Password': 'パスワード',
+    'Please confirm your Password': '確認用パスワードを入力してください',
+    'Nickname': 'ニックネーム',
+    'Enter your Nickname': 'ニックネームを入力してください',
+    'Your passwords must match': 'パスワードを合致させてください',
+    'Invalid verification code provided, please try again.': '確認コードに誤りがあるため、再度お試しください',
+    'Back to Sign In': 'ログイン画面に戻る',
+    'Cannot reset password for the user as there is no registered/verified email or phone_number': '会員登録されていないためパスワードリセットできません',
+  },
+});
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/user_edit" element={<UserEdit />} />
-      </Routes>
-    </BrowserRouter>
+    <Authenticator.Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/user_edit" element={<UserEdit />} />
+        </Routes>
+      </BrowserRouter>
+    </Authenticator.Provider>
   );
 }
 
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,16 +1,116 @@
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
+import { useTheme, View, Text, Heading, Button } from '@aws-amplify/ui-react';
 import MyPage from "./MyPage";
+
+// https://ui.docs.amplify.aws/react/connected-components/authenticator/customization
+const components = {
+  // 共通フッター
+  Footer() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Text color={tokens.colors.neutral[80]}>
+          &copy; Twin kangaroos All Rights Reserved
+        </Text>
+      </View>
+    );
+  },
+  // ログイン
+  // ━━━━━━━━━━
+  SignIn: {
+    // ログインのヘッダー
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={4}
+        >
+          ログイン
+        </Heading>
+      );
+    },
+    // ログインのフッター
+    Footer() {
+      const { toResetPassword } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <Button
+            fontWeight="normal"
+            onClick={toResetPassword}
+            size="small"
+            variation="link"
+          >
+            パスワードを忘れた方
+          </Button>
+        </View>
+      );
+    },
+  },
+  // 会員登録
+  // ━━━━━━━━━━
+  SignUp: {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={4}
+        >
+          Twin kangaroosに会員登録
+        </Heading>
+      );
+    },
+    Footer() {
+      const { toSignIn } = useAuthenticator();
+
+      return (
+        <View textAlign="center">
+          <Button
+            fontWeight="normal"
+            onClick={toSignIn}
+            size="small"
+            variation="link"
+          >
+            ログイン画面に戻る
+          </Button>
+        </View>
+      );
+    },
+  },
+  // 会員登録画面→確認コードの確認
+  // ━━━━━━━━━━
+  ConfirmSignUp: {
+    Header() {
+      const { tokens } = useTheme();
+      return (
+        <Heading
+          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+          level={3}
+        > 
+          確認コードの入力
+        </Heading>
+      );
+    },
+    Footer() {
+      return <Text></Text>;
+    },
+  },
+};
 
 const LoginPage = () => {
     return (
         <Authenticator signUpAttributes={[
             'email',
             'nickname',
-            ]}>
+            ]} components={components}
+        >
         {({ signOut, user }) => (
-            <Authenticator.Provider>
-                <MyPage />
-            </Authenticator.Provider>
+            <MyPage />
         )}
         </Authenticator>
       );
